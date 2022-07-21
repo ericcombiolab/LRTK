72b47b2af4bf34e9f38efc721a668d04172fc5d2
CicyYeung <yangchaogab@gmail.com>
2022-07-21 21:22:44 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,31 +20,11 @@ setup(
     long_description_content_type='text/markdown',
     author='YANG CHAO',
     author_email='yangchaogab@gmail.com',
-    python_requires='>=3.7.0',
     url='https://github.com/ericcombiolab/LRTK',
     packages=['lrtk'],
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
     install_requires=['pysam','pandas','bwa','picard','samtools','freebayes','hapcut2', 'aquila', 'vcflib'],
-    extras_require=EXTRAS,
+    entry_points={'console_scripts':['entry=packagename.scriptname:function',],
     include_package_data=True,
     license='MIT',
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-    ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    zip_safe=False
 )
